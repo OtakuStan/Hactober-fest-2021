09d12a0f512ef6d8528f50bdc75404e6e4648edb
Lunaticsatoshi <deysarkersudeep@gmail.com>
2021-10-01 21:45:30 +0530

    :package: Created and Added Hero section to app

--- src/sections/hero.tsx
+++ src/sections/hero.tsx
@@ -1,18 +1,36 @@
 import Image from "next/image";
-import Navbar from "src/components/Navbar";
+
 import styles from "styles/Hero.module.css";
 
 const Hero = () => {
   return (
-    <section className="hero" id="hero">
-      <Navbar />
+    <section id="hero">
       <div className={styles.hero}>
-        <Image
-          src="/images/hacktoberfest-logo.png"
-          alt="Hero Image"
-          width="500px"
-          height="200px"
-        />
+        <div className={styles.hero__container}>
+          <div className={styles.leftImage}>
+            <Image
+              src="/images/os-logo.png"
+              alt="Hero Image"
+              width="350px"
+              height="370px"
+            />
+          </div>
+          <div>
+            <h1>X</h1>
+          </div>
+          <div className={styles.rightImage}>
+            <Image
+              src="/images/hacktoberfest-logo.png"
+              alt="Hero Image"
+              width="500px"
+              height="200px"
+            />
+          </div>
+        </div>
+
+        <h4>An Open event for everyone to make positive contributions to an ever-growing developer community</h4>
+
+        <button className={styles.cta__btn}>Start Hacking</button>
       </div>
     </section>
   );
