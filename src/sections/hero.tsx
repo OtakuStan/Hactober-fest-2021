291dd97d4bc0fe2e12ba0e529404e64f75296f2c
Lunaticsatoshi <deysarkersudeep@gmail.com>
2021-10-01 21:00:32 +0530

    :camera: Adding Image to hero section

--- src/sections/hero.tsx
+++ src/sections/hero.tsx
@@ -1,11 +1,20 @@
-import React from "react";
+import Image from "next/image";
 import Navbar from "src/components/Navbar";
+import styles from "styles/Hero.module.css";
 
 const Hero = () => {
   return (
-    <>
+    <section className="hero" id="hero">
       <Navbar />
-    </>
+      <div className={styles.hero}>
+        <Image
+          src="/images/hacktoberfest-logo.png"
+          alt="Hero Image"
+          width="500px"
+          height="200px"
+        />
+      </div>
+    </section>
   );
 };
 
