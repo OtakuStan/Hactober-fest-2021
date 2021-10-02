f3da24619ad2dcfe473a07716e9ce8b9fa5992e5
Lunaticsatoshi <deysarkersudeep@gmail.com>
2021-10-02 17:10:08 +0530

    :package: Updating Navbar component

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -16,7 +16,7 @@ const Navbar = () => {
               </a>
             </li>
             <li className="nav-item">
-              <a className="nav-link" href="#">
+              <a className="nav-link" href="#contributors">
                 Contributers
               </a>
             </li>
