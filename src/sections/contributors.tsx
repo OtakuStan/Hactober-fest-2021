8c5e01021c2c6d38fd31510cd2395ef3d7c8f068
Lunaticsatoshi <deysarkersudeep@gmail.com>
2021-10-02 11:47:29 +0530

    :package: Adding styling to contributors section

--- src/sections/contributors.tsx
+++ src/sections/contributors.tsx
@@ -1,9 +1,11 @@
-
+import styles from "styles/Contributors.module.css";
 
 const contributors = () => {
     return (
         <section id="contributors">
-            <div className="contributors__section"></div>
+            <div className={styles.contributors__section}>
+                <div className={styles.posts}></div>
+            </div>
         </section>
     )
 }
