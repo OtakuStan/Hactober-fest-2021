fa1521b573cbe1a3b39d4bcc7d392d6adaa7b6db
Lunaticsatoshi <deysarkersudeep@gmail.com>
2021-10-02 11:41:32 +0530

    :package: Creating contributors section

---
+++ src/sections/contributors.tsx
@@ -0,0 +1,11 @@
+
+
+const contributors = () => {
+    return (
+        <section id="contributors">
+            <div className="contributors__section"></div>
+        </section>
+    )
+}
+
+export default contributors
