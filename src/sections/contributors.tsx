8d75c8d3553f7a1a80c2d74efe647dcf7480b172
Lunaticsatoshi <deysarkersudeep@gmail.com>
2021-10-02 12:11:00 +0530

    :package: Adding profile component to contributors section

--- src/sections/contributors.tsx
+++ src/sections/contributors.tsx
@@ -1,13 +1,46 @@
 import styles from "styles/Contributors.module.css";
-
+import Profile from "src/components/Profile";
 const contributors = () => {
-    return (
-        <section id="contributors">
-            <div className={styles.contributors__section}>
-                <div className={styles.posts}></div>
-            </div>
-        </section>
-    )
-}
+  return (
+    <section id="contributors">
+      <div className={styles.contributors__section}>
+        <div className={styles.posts}>
+          <Profile
+            title="Lunaticsatoshi"
+            image="https://avatars.githubusercontent.com/u/47326452?v=4"
+            repos={4}
+            followers={2}
+            following={2}
+            url="https://api.github.com/users/Lunaticsatoshi"
+          />
+          <Profile
+            title="Lunaticsatoshi"
+            image="https://avatars.githubusercontent.com/u/47326452?v=4"
+            repos={4}
+            followers={2}
+            following={2}
+            url="https://api.github.com/users/Lunaticsatoshi"
+          />
+          <Profile
+            title="Lunaticsatoshi"
+            image="https://avatars.githubusercontent.com/u/47326452?v=4"
+            repos={4}
+            followers={2}
+            following={2}
+            url="https://api.github.com/users/Lunaticsatoshi"
+          />
+          <Profile
+            title="Lunaticsatoshi"
+            image="https://avatars.githubusercontent.com/u/47326452?v=4"
+            repos={4}
+            followers={2}
+            following={2}
+            url="https://api.github.com/users/Lunaticsatoshi"
+          />
+        </div>
+      </div>
+    </section>
+  );
+};
 
-export default contributors
+export default contributors;
